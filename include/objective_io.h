bc7d3676d484c0f65532e0691cb2b2c5901faf5f
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-25 21:12:22 -0300

    Removed objective_io

--- include/objective_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef OLD_OBJECTIVE_IO_H
-#define OLD_OBJECTIVE_IO_H
-
-#include "parameters_type.h"
-
-void _save_objective_file(const char *path, const char *file_name,
-		const int *fit, protein ** pop, const int *pop_size,
-		const int *generation, const input_parameters_t *in_para);
-#endif
