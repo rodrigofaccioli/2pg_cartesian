fa4e6afea9126f0976cb4fc2e11871ec363a8517
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-25 21:10:17 -0300

    Added patter in file

--- include/futil.h
+++ include/futil.h
@@ -1,7 +1,10 @@
-enum mode_files{fREAD,fWRITE,fAPPEND};
-typedef enum mode_files mode_files_t;
+#ifndef OLD_FUTIL_H
+#define OLD_FUTIL_H
 
 #include "enums.h"
+
+enum mode_files{fREAD,fWRITE,fAPPEND};
+typedef enum mode_files mode_files_t;
 
 char * path_join_file(const char *path, const char *f);
 /*Returns path_file which is union of path and file.
@@ -15,3 +18,5 @@ boolean_t file_is_empty(FILE *file_aux);
 void set_random_file_name(char *file_name,__const char *prefix,
 		__const char *sufix );
 boolean_t check_exists_file(const char *path_file_name);
+
+#endif
