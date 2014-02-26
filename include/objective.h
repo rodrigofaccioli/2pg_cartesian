086aa3f93b463c87859b76a073467675240ddecc
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-25 21:15:32 -0300

    Added functions

--- include/objective.h
+++ include/objective.h
@@ -3,6 +3,8 @@
 
 #include "enums.h"
 
-type_fitness_energies_t str2type_fitness_energies(char *name_fitness_energies);
+type_fitness_energies_t str2type_objective(char *name_fitness_energies);
+void type_fitness_energies2str(char *name_fitness_energies,
+		const type_fitness_energies_t *type_fitness );
 
 #endif
--- src/objective.c
+++ src/objective.c
@@ -4,7 +4,7 @@
 #include "objective.h"
 #include "messages.h"
 
-type_fitness_energies_t str2type_fitness_energies(char *name_fitness_energies){
+type_fitness_energies_t str2type_objective(char *name_fitness_energies){
 	if (strcmp(name_fitness_energies,"Potential") == 0){
 		return fit_ener_potential;
 	}else if (strcmp(name_fitness_energies,"Van_der_Waals") == 0){
@@ -36,5 +36,39 @@ type_fitness_energies_t str2type_fitness_energies(char *name_fitness_energies){
 		sprintf(msg,"The option %s did not find at type_fitness_energies_t. Please, check it!", name_fitness_energies);
 		fatal_error(msg);
 	}
+}
 
+void type_fitness_energies2str(char *name_fitness_energies,
+		const type_fitness_energies_t *type_fitness ){
+	if (*type_fitness == fit_ener_potential){
+		strcpy(name_fitness_energies,"Potential");
+	}else if (*type_fitness == fit_ener_edw){
+		strcpy(name_fitness_energies,"Van_der_Waals");
+	}else if (*type_fitness == fit_ener_ele){
+		strcpy(name_fitness_energies,"Electrostatic");
+	}else if (*type_fitness == fit_hydrophobic){
+		strcpy(name_fitness_energies,"Hydrophobic");
+	}else if (*type_fitness == fit_hydrophilic){
+		strcpy(name_fitness_energies,"Hydrophilic");
+	}else if (*type_fitness == fit_total_area){
+		strcpy(name_fitness_energies,"Total_Area");
+	}else if (*type_fitness == fit_gyrate){
+		strcpy(name_fitness_energies,"Gyrate");
+	}else if (*type_fitness == fit_hbond){
+		strcpy(name_fitness_energies,"H_Bond");
+	}else if (*type_fitness == fit_hbond_main){
+		strcpy(name_fitness_energies,"H_Bond_Main");
+	}else if (*type_fitness == fit_GBSA_Solvatation){
+		strcpy(name_fitness_energies,"GBSA_Solvatation");
+	}else if (*type_fitness == fit_stride_total){
+		strcpy(name_fitness_energies,"Stride_total");
+	}else if (*type_fitness == fit_stride_helix){
+		strcpy(name_fitness_energies,"Stride_helix");
+	}else if (*type_fitness == fit_stride_beta){
+		strcpy(name_fitness_energies,"Stride_beta");
+	}else{
+		char msg[1024];
+		sprintf(msg,"The option did not find at type_fitness_energies2str function. Please, check it!");
+		fatal_error(msg);
+	}
 }
