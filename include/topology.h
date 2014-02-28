df763ceab9423544f3cf3423fecb6390c460522f
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-28 15:43:53 -0300

    build_topology_individual function added in .h

--- include/topology.h
+++ include/topology.h
@@ -12,6 +12,7 @@ top_global_t *allocateTop_Global(const primary_seq_t *primary_sequence,
 
 void  desAllocateTop_Global(top_global_t *top_aux);
 void build_topology_population(protein_t *pop, const int *pop_size);
+void build_topology_individual(protein_t *prot);
 int get_number_hydrogen_backbone(const protein_t *prot, const int *res);
 boolean_t is_hydrogen_backbone_Nitrogen(const char *atomname);
 boolean_t is_fixed_atom(const int *atmnumber, const int *fixed_atoms, const int *num_fixed);
