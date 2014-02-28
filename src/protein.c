4f91cd3163f2316ce8407addc6738045cb6ba94c
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-28 15:21:10 -0300

    Added functions to copy proteins

--- src/protein.c
+++ src/protein.c
@@ -43,3 +43,39 @@ void set_proteins2solutions(solution_t *sol, protein_t *pop, const int *pop_size
 		sol[i].representation = &pop[i];
 	}
 }
+
+/** Copies p_source to p_dest */
+void copy_protein(protein_t *p_dest, const protein_t *p_source){
+	//Coping atoms
+	for (int a = 0; a < p_source->p_topol->numatom; a++){			
+		copy_pdb_atom(&p_dest->p_atoms[a], &p_source->p_atoms[a]);
+	}		
+	//Building Topology of p_dest
+	build_topology_individual(p_dest);
+}
+
+/** Copies each individual of pop_source to pop_dest */
+void copy_protein_population(protein_t *pop_dest, const protein_t *pop_source, 
+	const int *popsize){
+	for (int i = 0; i < *popsize; i++){
+		copy_protein(&pop_dest[i], &pop_source[i]);
+	}
+}
+
+/** Copies atoms of p_source to p_dest */
+void copy_protein_atoms(protein_t *p_dest, const protein_t *p_source){
+	//Coping atoms
+	for (int a = 0; a < p_source->p_topol->numatom; a++){			
+		copy_pdb_atom(&p_dest->p_atoms[a], &p_source->p_atoms[a]);
+	}
+}
+
+/** Copies atoms of each individual of pop_source to pop_dest */
+void copy_protein_population_atoms(protein_t *pop_dest, const protein_t *pop_source, 
+	const int *popsize){
+	for (int i = 0; i < *popsize; i++){
+		copy_protein_atoms(&pop_dest[i], &pop_source[i]);
+	}
+}
+
+
