e64a49b27412df0354d88a168550df58b3dbfa78
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-02-28 15:52:42 -0300

    EA Mono-objective working fine

--- include/ea_mono.h
+++ include/ea_mono.h
@@ -1,9 +1,7 @@
 #ifndef OLD_EA_MONO_H
 #define OLD_EA_MONO_H
 
-#include "protein.h"
-#include "ea_types.h"
-#include "topology_types.h"
+#include "parameters_type.h"
 
 int ea_mono(const input_parameters_t *in_para);
 
--- src/ea_mono.c
+++ src/ea_mono.c
@@ -1,18 +1,16 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "defines.h"
 #include "enums.h"
-#include "parameters_type.h"
 #include "ea_mono.h"
 #include "protein.h"
 #include "topology.h"
 #include "pdbio.h"
-//#include "energy_gromacs.h"
 #include "pdbatom.h"
 #include "messages.h"
-#include "ea_types.h"
-#include "ea.h"
+#include "algorithms.h"
 #include "string_owner.h"
 #include "futil.h"
 #include "random_number_gsl.h"
@@ -24,139 +22,221 @@
 #include "rotation.h"
 #include "math_owner.h"
 #include "solution.h"
+#include "gromacs_objectives.h"
+#include "algorithms.h"
+#include "randomlib.h"
 
+ /** Implementation of tournament for chosing individual to reproduce 
+ * solutions represents the population of solutions
+ * in_para represents the parameters
+ * output is the reference of solution has earned the tournament
+ */
+static int tournament(const solution_t *solutions, const int *popsize){
+    int ind_1, ind_2;
+    int max_random;
+    max_random = *popsize -1;
+    ind_1 = _get_int_random_number(&max_random);
+    ind_2 = _get_int_random_number(&max_random);
+    if (solutions[ind_1].obj_values[0] < solutions[ind_2].obj_values[0]){
+        return ind_1;
+    }else{
+        return ind_2;
+    }
+}
 
+/** Selects the individuals which will be reproduced 
+* index_solutions_to_reproduce index of individuals were selected to reproduce
+* num_ind_reproduce number the individuals for reprodution
+* solutions population of solutions
+* popsize size of population
+*/
+static void select_individual_mono(int *index_solutions_to_reproduce, 
+        const int *num_ind_reproduce, const solution_t *solutions, const int *popsize){    
+    for (int i = 0; i < *num_ind_reproduce;i++){
+       index_solutions_to_reproduce[i] = tournament(solutions,popsize);
+    }
 
-static void check_parameters(const input_parameters_t *in_para){
-    /*Check the parameters to run EA mono*/
-	if (in_para->number_individual_select_reproduce > in_para->size_population){
-		fatal_error("The parameter NumberIndividualReproduce must be smaller than SizePopulation. Please check it!");
-	}
+}
+
+/** Applies the crossover parents.
+* p_new is based on one of its parents
+* When choose = 0 is considered father 1. Otherwise, father 2
+*/
+void crossover_parents(protein_t *p_new, const protein_t *p1,
+        const protein_t *p2){
+    int choose;
+    int aux = 2;
+    choose = _get_int_random_number(&aux);
+    if (choose == 0){        
+        copy_protein(p_new,p1);
+    }else{
+        copy_protein(p_new,p2);
+    }
+}
+
+/** Applies the crossover operator
+* ind_new is an individual of new population
+* prot_1 is first indiviual
+* prot_2 is second indiviual
+*/
+void apply_crossover(protein_t *ind_new, const protein_t *prot_1, 
+    const protein_t * prot_2){
+
+    crossover_parents(ind_new, prot_1, prot_2);
+}
+
+/** Applies the mutation operator
+* ind_new is an individual of new population
+* in_para is the input parameter
+*/
+void apply_mutation(protein_t *ind_new, const input_parameters_t *in_para){
+    float angle_radians, angle_degree, rate;
+    int num_residue_choose;
+    int what_rotation;
+    int max_kind_of_rotation = 3;  
+
+    rate = _get_float();
+    num_residue_choose = 0;
+    if (rate < in_para->individual_mutation_rate){
+        //Choose a residue. It must be started 1 untill number of residue
+        while (num_residue_choose == 0){
+            num_residue_choose = _get_int_random_number(&ind_new->p_topol->numres);
+        }
+        //Obtaing kind of rotation
+        what_rotation = _get_int_random_number(&max_kind_of_rotation);
+        //Appling the rotation 
+        if (what_rotation == 0){
+            //Obtaing a random degree angule 
+            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_psi, 
+            &in_para->max_angle_mutation_psi);
+            //Cast to radians
+            angle_radians = degree2radians(&angle_degree);
+            rotation_psi(ind_new, &num_residue_choose, &angle_radians);
+        }else if (what_rotation == 1){
+            //Obtaing a random degree angule 
+            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_phi, 
+            &in_para->max_angle_mutation_phi);
+            //Cast to radians
+            angle_radians = degree2radians(&angle_degree);
+            rotation_phi(ind_new, &num_residue_choose, &angle_radians);            
+        }else if (what_rotation == 2){
+            //Obtaing a random degree angule 
+            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_omega, 
+            &in_para->max_angle_mutation_omega);
+            //Cast to radians
+            angle_radians = degree2radians(&angle_degree);
+            rotation_omega(ind_new, &num_residue_choose, &angle_radians);            
+        }
+    }
+
+}
+
+/** Applies the reprodution in proteins that are into solutions
+* pop_new indicates the new population
+* solutions contain current protein through the field called representation 
+* index_solutions_to_reproduce shows the index of selected solutions
+* in_para stores the input parameters
+* size_to_reproduce indicates the size of population to reproduce. It can be
+                    equals with population size
+*/
+void reproduce_protein(protein_t *pop_new, const solution_t *solutions, 
+    const int *index_solutions_to_reproduce, 
+    const input_parameters_t *in_para, const int *size_to_reproduce){
+
+    protein_t *prot_aux_1, *prot_aux_2;
+    int index_aux, ind_1, ind_2, max_random;
+
+    max_random = in_para->size_population -1;
+    for (int i = 0; i < *size_to_reproduce; i++){
+        //Obtaining two solutions from solutions that will be performed by genetic operators
+        index_aux = _get_int_random_number(&max_random);
+        ind_1 = index_solutions_to_reproduce[index_aux];
+        index_aux = _get_int_random_number(&max_random);
+        ind_2 = index_solutions_to_reproduce[index_aux];        
+        //Obtaining the proteins that were choosen from solutions
+        prot_aux_1 = (protein_t*) solutions[ind_1].representation;
+        prot_aux_2 = (protein_t*) solutions[ind_2].representation;        
+        //Appling crossover operator between prot_aux_1 and prot_aux_2. Resulting pop_new[i]
+        apply_crossover(&pop_new[i], prot_aux_1, prot_aux_2);        
+        //Appling mutation operator in pop_new[i]        
+        apply_mutation(&pop_new[i], in_para);
+    }
+
 }
 
 int ea_mono(const input_parameters_t *in_para){
-	check_parameters(in_para);
 
     char *prefix;
     primary_seq_t *primary_sequence; // Primary Sequence of Protein
-    protein_t *population_p;    
-    solution_t *solutions_p;
+    
+    protein_t *population_p; // main population
+    protein_t *population_new; //population obtained by reprodution
+
+    solution_t *solutions_p; // main solution
+    int *index_solutions_to_reproduce; // stores the index of solutions that were selected to reproduce
 
     //Loading Fasta file
     primary_sequence = _load_amino_seq(in_para->seq_protein_file_name);
 
     //Allocating PDB ATOMS
     population_p = allocateProtein(&in_para->size_population);
+    population_new = allocateProtein(&in_para->size_population);
     
     //Loading initial population
     load_initial_population_file(population_p, &in_para->size_population, 
+        in_para->path_local_execute,in_para->initial_pop_file_name,
+        primary_sequence);
+    load_initial_population_file(population_new, &in_para->size_population, 
         in_para->path_local_execute,in_para->initial_pop_file_name,
         primary_sequence);
 
     //Saving topology of population 
     prefix = Malloc(char,10);
     strcpy(prefix, "prot");
-    save_topology_population(population_p, &in_para->size_population, 
-    in_para->path_local_execute, prefix);    
+    //save_topology_population(population_p, &in_para->size_population, 
+    //in_para->path_local_execute, prefix);
+    save_topology_population(population_new, &in_para->size_population, 
+    in_para->path_local_execute, prefix);        
     free(prefix);
 
+/**************** STARTING Mono-Objetive Evolutionary Algorithm *************************/
+    display_msg("Starting Mono-Objetive Evolutionary Algorithm \n");
+    initialize_algorithm_execution(primary_sequence, in_para);
+    index_solutions_to_reproduce = Malloc(int, in_para->size_population);
+
     //Setting solutions
-    solutions_p = allocate_solution(&in_para->size_population, &in_para->number_fitness);
+    solutions_p = allocate_solution(&in_para->size_population, &in_para->number_fitness);        
+    //Setting reference of proteins to solution 
     set_proteins2solutions(solutions_p, population_p, &in_para->size_population);
-    
-/**** TESTE ROTACAO ***************************************************/
-    float angle_d, angle;
-    int num_res_first;
-    char *file_name, *file_name_aux;
-    file_name = Malloc(char, 100);
-    file_name_aux= Malloc(char, 100);
+    //Computing objectives of solutions with GROMACS
+    get_gromacs_objectives(solutions_p, in_para);
+    for (int g = 1; g <= in_para->number_generation; g++){        
+        //Seleting solutions (individuals) which will be reproduced. Now, all population will be reproduced
+        select_individual_mono(index_solutions_to_reproduce, &in_para->size_population, 
+            solutions_p, &in_para->size_population);
+        //Reprodution of population - Building new proteins based on selected individuals
+        reproduce_protein(population_new, solutions_p, index_solutions_to_reproduce, 
+            in_para, &in_para->size_population);        
+        //Updating the atoms of population_p from population_new
+        copy_protein_population_atoms(population_p, population_new, &in_para->size_population);        
+        //Saving information of generation
+        update_execution_algorithms(solutions_p, &g);        
+        /* Computing objectives of solutions with GROMACS since population_p was 
+         * updated by new population (generated by reprodution) */
+        get_gromacs_objectives(solutions_p, in_para);
+    }
+    // Sorting solutions_p
 
-    angle_d = 0;    
-    for (int p = 0; p < in_para->size_population; p++){
-        if (angle_d >= 360){
-            angle_d = 0;    
-        } 
-        angle_d = angle_d + 10;
-        angle = degree2radians(&angle_d);
-        num_res_first = 21;
-        //rotation_psi(&population_p[p], &num_res_first, &angle);
-        //rotation_phi(&population_p[p], &num_res_first, &angle);
-        rotation_omega(&population_p[p], &num_res_first, &angle);
-    }    
-    sprintf(file_name_aux,"1VII_2PG_%i.pdb",num_res_first);
-    strcpy(file_name, file_name_aux);
-    save_population_file(population_p, in_para->path_local_execute,
-            file_name, &in_para->size_population );
-/**********************************************************************/
+    // Saving sorted population
 
+    free(index_solutions_to_reproduce);
+/**************** FINISHED Mono-objetive Evolutionary Algorithm *************************/     
 
     desallocate_solution(solutions_p, &in_para->size_population);
     desallocateProtein(population_p, &in_para->size_population);
     desallocate_primary_seq(primary_sequence);
+    _finish_random_gsl();
 
-
-/*    
-    display_msg("Build Topology \n");
-	top_global = allocateTop_Global(&numatom, &nresiduos,&bond_angles,
-			&num_protein_side_chains,&num_dihedral_angles_type);
-    build_top_global(primary_sequence,top_global);
-    save_topology(in_para->path_local_execute, in_para->top_file,top_global);
-
-    display_msg("Allocating populations \n");
-	population_p = allocatePopulation(in_para->size_population,nresiduos,
-			in_para->number_fitness, top_global->numatom);
-	population_new = allocatePopulation(in_para->size_population,nresiduos,
-			in_para->number_fitness, top_global->numatom);
-	population_sel_repro = allocatePopulation(in_para->number_individual_select_reproduce,
-			nresiduos, in_para->number_fitness, top_global->numatom);
-
-	display_msg("Reading population \n");
-    readPopulationFile(population_p, &in_para->size_population,
-    		in_para->path_local_execute,in_para->initial_pop_file_name,
-    		primary_sequence,&nresiduos, top_global);
-
-    display_msg("Starting Evolutionary Algorithm Mono-Objective \n");
-    ea_initialize(primary_sequence, &nresiduos, top_global, in_para);
-	initialize_fitness_in_file(in_para->path_local_execute,
-			fitness_file_name);
-    for (int g = 1; g <= in_para->number_generation;g++){
-    	show_generation_msg(&g);
-    	fitness_gromacs(population_p,in_para, top_global);
-    	set_fitness_in_file(in_para->path_local_execute,fitness_file_name,
-    			population_p,&in_para->size_population,&g);
-    	select_individual_mono(population_sel_repro, population_p, &nresiduos,
-    			in_para);
-    	reproduce_population(population_new,population_sel_repro,in_para);
-    	display_msg("Building random individuals \n");
-    	build_random_individuals_mono(population_new,&in_para->size_population,
-    			&in_para->number_individual_select_reproduce);
-    	update_generation(population_p, &g);
-    	/* population_p is updated until g < number_generation
-    	 * because population_p is used to generate the final results and the
-    	 * population_new will not be evaluated. So, We need to maintain the
-    	 * population_p unchanged.
-    	 */
-/*    	 
-    	if (g < in_para->number_generation){
-    		copy_population_without_allocating(population_p,population_new,
-    				&in_para->size_population);
-    	}
-
-    }
-    display_msg("Generating Final Results \n");
-    build_final_results(population_p,in_para, primary_sequence,
-    		top_global);
-
-    display_msg("Deallocating variables \n");
-    ea_finished();
-    deAllocateAmino(primary_sequence,nresiduos);
-    desAllocateTop_Global(top_global);
-    deAllocatePopulation(population_p, in_para->size_population);
-    deAllocatePopulation(population_new, in_para->size_population);
-    deAllocatePopulation(population_sel_repro, in_para->
-    		number_individual_select_reproduce);
-    finish_random();
-*/    
 	return 0;
 }
 
