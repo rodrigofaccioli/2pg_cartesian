6fd0511f3d5326a32ac9d7a8a6c135e5c48ac335
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-04-28 03:18:17 -0300

    Replaced functions that are used to apply genetic operators

--- src/ea_mono.c
+++ src/ea_mono.c
@@ -77,30 +77,6 @@ static void build_final_results(solution_t *pop,const input_parameters_t *in_par
 }
 
 
-/** Implementation of 1 point crossover
-* p_new represents the new solution
-* p1 means the solution 1 that will build s_new
-* p2 means the solution 2 that will build s_new
-*/
-static void crossover_1_part(protein_t *p_new, const protein_t *p1, 
-    const protein_t *p2){
-    int res_aux, res_ini;
-    res_aux = 0;
-    res_ini = 1;
-
-    //getting a random residue
-    res_aux = get_choose_residue(&p1->p_topol->numres);
-    //coping from solution s1
-    copy_protein_atoms_by_residues(p_new, &res_ini, &res_aux, p1);
-    //coping from solution s2
-    res_ini = res_aux + 1;
-    if (res_ini <= p2->p_topol->numres){
-        res_aux = p2->p_topol->numres;
-        copy_protein_atoms_by_residues(p_new, &res_ini, &res_aux, p2);
-    }
-    build_topology_individual(p_new);
-}
-
  /** Implementation of tournament for chosing individual to reproduce 
  * solutions represents the population of solutions
  * in_para represents the parameters
@@ -133,112 +109,6 @@ static void select_individual_mono(int *index_solutions_to_reproduce,
 
 }
 
-/** Applies the crossover parents.
-* p_new is based on one of its parents
-* When choose = 0 is considered father 1. Otherwise, father 2
-*/
-void crossover_parents(protein_t *p_new, const protein_t *p1,
-        const protein_t *p2){
-    int choose;
-    int aux = 2;
-    choose = _get_int_random_number(&aux);
-    if (choose == 0){        
-        copy_protein_atoms(p_new,p1);
-    }else{
-        copy_protein_atoms(p_new,p2);
-    }
-}
-
-/** Applies the crossover operator
-* ind_new is an individual of new population
-* prot_1 is first indiviual
-* prot_2 is second indiviual
-*/
-void apply_crossover(protein_t *ind_new, const protein_t *prot_1, 
-    const protein_t * prot_2, type_crossoers_t *crossovers){
-    if (crossovers[0] != crossoer_none){
-        int choose;
-        int aux = 2;
-        choose = _get_int_random_number(&aux);
-        if (choose == 0){
-            crossover_parents(ind_new, prot_1, prot_2);
-        }else{
-            crossover_1_part(ind_new, prot_1, prot_2);
-        }
-    }else{
-        crossover_parents(ind_new, prot_1, prot_2);
-    }
-}
-
-/** Applies the mutation operator
-* ind_new is an individual of new population
-* in_para is the input parameter
-*/
-void apply_mutation(protein_t *ind_new, const input_parameters_t *in_para){
-    float angle_radians, angle_degree, rate;
-    int num_residue_choose;
-    int what_rotation;
-    int max_kind_of_rotation = 4;  
-
-    rate = _get_float();
-    num_residue_choose = 0;
-    if (rate < in_para->individual_mutation_rate){
-        //Choose a residue
-        num_residue_choose = get_choose_residue(&ind_new->p_topol->numres);
-        //Obtaing kind of rotation
-        what_rotation = _get_int_random_number(&max_kind_of_rotation);        
-        //Appling the rotation 
-        if (what_rotation == 0){
-            //Obtaing a random degree angule 
-            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_psi, 
-            &in_para->max_angle_mutation_psi);
-            //Cast to radians
-            angle_radians = degree2radians(&angle_degree);
-            rotation_psi(ind_new, &num_residue_choose, &angle_radians);
-        }else if (what_rotation == 1){
-            //Obtaing a random degree angule 
-            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_phi, 
-            &in_para->max_angle_mutation_phi);
-            //Cast to radians
-            angle_radians = degree2radians(&angle_degree);
-            rotation_phi(ind_new, &num_residue_choose, &angle_radians);            
-        }else if (what_rotation == 2){
-            //Obtaing a random degree angule 
-            angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_omega, 
-            &in_para->max_angle_mutation_omega);
-            //Cast to radians
-            angle_radians = degree2radians(&angle_degree);
-            rotation_omega(ind_new, &num_residue_choose, &angle_radians);            
-        }else if (what_rotation == 3){
-            int chi = 0;
-            int max_chi = -1;
-            char *res_name;
-            res_name = Malloc(char, 4);
-            get_res_name_from_res_num(res_name, &num_residue_choose, 
-                ind_new->p_atoms, &ind_new->p_topol->numatom);
-            max_chi = get_number_chi(res_name);                        
-            if (max_chi > 0){
-                //Choose a chi of residue. It must be started 1 untill number of residue
-                if (max_chi == 1){
-                    chi = 1;
-                }else{
-                    while (chi == 0){                
-                        chi = _get_int_random_number(&max_chi);
-                    }                    
-                }                
-                //Obtaing a random degree angule 
-                angle_degree = _get_float_random_interval(&in_para->min_angle_mutation_side_chain, 
-                &in_para->max_angle_mutation_side_chain);
-                //Cast to radians
-                angle_radians = degree2radians(&angle_degree);
-                rotation_chi(ind_new, &num_residue_choose, &chi, &angle_radians);
-            }
-            free(res_name);
-        }
-    }
-
-}
-
 /** Applies the reprodution in proteins that are into solutions
 * pop_new indicates the new population
 * solutions contain current protein through the field called representation 
@@ -265,7 +135,7 @@ void reproduce_protein(protein_t *pop_new, const solution_t *solutions,
         prot_aux_1 = (protein_t*) solutions[ind_1].representation;
         prot_aux_2 = (protein_t*) solutions[ind_2].representation;        
         //Appling crossover operator between prot_aux_1 and prot_aux_2. Resulting pop_new[i]
-        apply_crossover(&pop_new[i], prot_aux_1, prot_aux_2, in_para->crossovers);        
+        apply_crossover(&pop_new[i], prot_aux_1, prot_aux_2, in_para->crossovers);
         //Appling mutation operator in pop_new[i]        
         apply_mutation(&pop_new[i], in_para);
     }
