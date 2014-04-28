01223902e4bc457755436cd4bc628782b1fc0626
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-04-28 02:50:16 -0300

    Added functions to increase more flexibilit

--- include/solution.h
+++ include/solution.h
@@ -3,8 +3,10 @@
 
 #include "solution_types.h"
 
+void initialize_solution(solution_t * solution_aux, const int *nbj);
 solution_t * allocate_solution(const int *nsol, const int *nbj);
 void desallocate_solution(solution_t *sol, const int *nsol);
+void copy_solution_objectivies(solution_t *dest, const solution_t *source);
 double get_specific_objective(const solution_t *sol, const int *obj);
 double get_oposite_specific_objective(const solution_t *sol, const int *obj);
 
--- src/solution.c
+++ src/solution.c
@@ -4,6 +4,17 @@
 #include "solution.h"
 #include "defines.h"
 
+
+/** Initialize a Solution
+* solution_aux represents the solution which will be initialized
+* nbj represents the number of objectivies
+*/
+void initialize_solution(solution_t * solution_aux, const int *nbj){
+		solution_aux->num_obj = *nbj;
+		solution_aux->obj_values = Malloc(double, solution_aux->num_obj);
+		solution_aux->representation = NULL;
+}
+
 /** Allocates an array of solution
 * nsol is the number of solutions. Its number will be population size
 * nbj is the number of objectives
@@ -13,9 +24,7 @@ solution_t * allocate_solution(const int *nsol, const int *nbj){
 	solution_aux = Malloc(solution_t,*nsol);
 	//Each solution
 	for (int s = 0; s < *nsol; s++){
-		solution_aux[s].num_obj = *nbj;
-		solution_aux[s].obj_values = Malloc(double, solution_aux[s].num_obj);
-		solution_aux[s].representation = NULL;
+		initialize_solution(&solution_aux[s], nbj);
 	}
 	return solution_aux;
 }
@@ -25,6 +34,15 @@ void desallocate_solution(solution_t *sol, const int *nsol){
 		free(sol[s].obj_values);
 	}
 	free(sol);
+}
+
+/** Copy all objectivies between two solutions
+*/ 
+void copy_solution_objectivies(solution_t *dest, const solution_t *source){
+    dest->num_obj = source->num_obj;
+    for (int obj = 0; obj < dest->num_obj; obj++){
+    	dest->obj_values[obj] = source->obj_values[obj];
+    }
 }
 
 /** Returns the value of an specific objective */
