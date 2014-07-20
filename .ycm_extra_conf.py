c74db611df2272dd1960481a51962d6fb2535f90
Tarcísio G. Rodrigues <tarcisio.genaro@gmail.com>
2014-07-20 02:01:55 -0300

    Adding youcompleteme settings for linux

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,63 @@
+import os
+import ycm_core
+
+flags = [
+'-Wall',
+'-Wextra',
+'-Wc++98-compat',
+'-Wno-long-long',
+'-Wno-variadic-macros',
+'-fexceptions',
+'-DNDEBUG',
+'-std=c++11',
+'-x', 'c++',
+'-I', './include',
+'-I', './src',
+'-isystem', '/usr/include',         # Boost and OpenSSL are here
+'-isystem', '/usr/local/include',   # CppCMS is here
+
+# Without the two below, nothing works
+'-isystem', '/usr/lib/llvm-3.4/include',
+'-isystem', '/usr/lib/clang/3.4/include'
+]
+
+def DirectoryOfThisScript():
+  return os.path.dirname( os.path.abspath( __file__ ) )
+
+def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
+  if not working_directory:
+    return list( flags )
+  new_flags = []
+  make_next_absolute = False
+  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+  for flag in flags:
+    new_flag = flag
+
+    if make_next_absolute:
+      make_next_absolute = False
+      if not flag.startswith( '/' ):
+        new_flag = os.path.join( working_directory, flag )
+
+    for path_flag in path_flags:
+      if flag == path_flag:
+        make_next_absolute = True
+        break
+
+      if flag.startswith( path_flag ):
+        path = flag[ len( path_flag ): ]
+        new_flag = path_flag + os.path.join( working_directory, path )
+        break
+
+    if new_flag:
+      new_flags.append( new_flag )
+  return new_flags
+
+
+def FlagsForFile( filename, **kwargs ):
+  relative_to = DirectoryOfThisScript()
+  final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
+
+  return {
+    'flags': final_flags,
+    'do_cache': True
+  }
